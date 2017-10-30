f9b676e36a8973452da0cc81d1f0a72af2e1d3db
Marco Vanali <Marco91Vanali@gmail.com>
2017-10-30 17:03:58 +0000

    Created Oystercard class, a balance in the initialize method and its test to check if a Oystercard.new has a balance.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ ruby '2.4.2'
 group :development, :test do
   gem 'rspec'
   gem 'rubocop'
+  gem 'rake'
+  gem 'reek'
+  gem 'simplecov', require: false, group: :test
+  gem 'simplecov-console', require: false, group: :test
 end
 
 #gem install bundler
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,13 @@
 require 'oystercard'
 
 describe Oystercard do
+
+subject(:oystercard) { described_class.new }
+
+  describe 'initialize' do
+    it 'Check if oystercard has a balance equal to 0' do
+      expect(subject.balance).to eq(0)
+    end
+  end
+
 end
